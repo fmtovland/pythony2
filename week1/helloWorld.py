224c4e124620d2de62629303d94b739b5e0b091c
Liam Mc Cormick <ljmc2000@gmail.com>
2017-09-14 14:22:31 +0100

    added my first ever python programme (not really, see year1 labtest1.py)

---
+++ week1/helloWorld.py
@@ -0,0 +1,11 @@
+#Program Discription: My first python programme
+#Programme Author: Liam Mc Cormick
+#Date: 14/9/17
+#python version: 3.4.5
+#OS: Gentoo
+#Kernel: linux 4.9.48
+
+print("Hello World")
+
+#Thats a lot of intro for a pretty basic program huh...
+#ah well, good to keep up good habbits...
