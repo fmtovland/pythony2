f2f561e5a6b16f1d9e063b0baf8589e9f4a54892
Liam Mc Cormick <ljmc2000@gmail.com>
2017-09-19 18:26:00 +0100

    added better comments

--- week0/b2i.py
+++ week0/b2i.py
@@ -2,19 +2,20 @@
 
 binaryNum=input("Enter a binary number: ")
 
-j=len(binaryNum) - 1
-i=0
-output=0
+j=len(binaryNum) - 1	#end condition for loop: count number of 1s and 0s in binary string
+i=0			#counter for loop
+output=0		#create empty output intager
 while(i<=j):
 	if binaryNum[i]=='1':
-		output=output+(2**(j-i))
+		output=output+(2**(j-i))	#if 1 entered in nth slot, add 2 to the power of n to total
 	elif binaryNum[i]=='0':
-		output=output		#do nothing
-	else:
+		output=output			#otherwise, do nothing
+
+	else:	#detect invalid input and exit gracelessly
 		print("Error: Non binary number detected")
 		print(binaryNum[i])
 		quit(1)
 
-	i=i+1
+	i=i+1	#increment counter
 
 print(output)
--- week0/i2b.py
+++ week0/i2b.py
@@ -9,24 +9,24 @@ number=int(number)
 
 biggest=int(log2(number))	#find how many 1s and 0s are in the output
 print(biggest)
-output=[]
+output=[]			#an empty list, which will be populated with 1s and 0s
 
 while(biggest>=0):
 
 	if(number-(2**biggest) >= 0):		#biggest at this point is which digit of binary you are currently calculating
-		number=number-(2**biggest)
-		output.insert(0,'1')
+		number=number-(2**biggest)	#subtract the power of two which has already been converted to a binary digit
+		output.insert(0,'1')		#add a 1 to the string output
 
 	else:
-		output.insert(0,'0')
+		output.insert(0,'0')		#add a 0 to the string output
 
-	biggest=biggest-1
+	biggest=biggest-1			#decrement the loops condition
 
 print("in binary that is",end=' ')
 
 i=len(output)
-while i>0:
+while i>0:	#print the string of binary, backwards
 	i=i-1
 	print(output[i],end='')
 
-print("")
+print("")	#print one last \n
