ff0ce84007660cf5bd5e9643e9f18c08d9d744da
Liam Mc Cormick <ljmc2000@gmail.com>
2017-09-21 15:15:33 +0100

    commented out debugging code

--- week1/2anybase.py
+++ week1/2anybase.py
@@ -17,7 +17,7 @@ while output[i]>=base:
 	while output[i]>=base:
 		output[i+1]=output[i+1]+1
 		output[i]=output[i]-base
-		print(output)			#for debugging
+#		print(output)			#for debugging
 	i=i+1
 
 #print(number,"in base",base,"is",output)	#good for debug, but prints backward
