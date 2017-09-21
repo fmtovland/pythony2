6b43fc2c54d0f90b4af1a8c47ea7bfb05d3c5de1
Liam Mc Cormick <ljmc2000@gmail.com>
2017-09-21 14:51:25 +0100

    no longer prints output backward

--- week1/2anybase.py
+++ week1/2anybase.py
@@ -14,4 +14,13 @@ while output[i]>base:
 		output[i+1]=output[i+1]+1
 		output[i]=output[i]-base
 
-print(number,"in base",base,"is",output)
+#print(number,"in base",base,"is",output)	#good for debug, but prints backward
+
+#print the output reversed
+i=len(output) - 1
+while i>= 0:
+	print(output[i],end='')
+	i=i-1
+
+#final \n
+print("")
