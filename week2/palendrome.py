60e147fd8b69a575322a1a4eb1feef74d319a03c
Liam Mc Cormick <ljmc2000@gmail.com>
2017-09-28 14:22:34 +0100

    changed () to [] in bad list. the error changed

--- week2/palendrome.py
+++ week2/palendrome.py
@@ -2,7 +2,7 @@ word=input("Enter word to check if palendrome: ")
 i=0
 wordlen=(len(word)/2)
 while(i < wordlen):
-	if(word(wordlen-i) != word(i)):
+	if word[wordlen-i] != word[i]:
 		print("Thats not a palendrome")
 		exit()
 	i=i+1
