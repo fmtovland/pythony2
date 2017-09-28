d629895dba50b1b98b7d7709d0a122673b9928e0
Liam Mc Cormick <ljmc2000@gmail.com>
2017-09-28 15:31:43 +0100

    santaxaclly broken

--- week2/scramble_word.py
+++ week2/scramble_word.py
@@ -1,6 +1,27 @@
-#Program requirements: Scramble a word
+#Program requirements: Scramble a word/words in sentence.
 #Program Author: Liam Mc Cormick
 #Date: 28/09/17
 #OS: Gentoo
 #Kernel: linux 4.9.49-gentoo-r1
 #python version: 3.4.5
+
+user_input=input("Enter string to have it scrambled\n")	#get user input
+i=0	#index of first letter of word
+j=0	#index of space after word
+
+while i < len(user_input):	#program will loop till i reaches end
+	while user_input[j] != ' ' or '.' or ',':	#after this user_input[j] will be the last letter of word
+		j=j+1
+	j=j-1
+
+	print(user_input[i])	#print first letter of word
+
+	k=0+j	#ensure k not directly mapped to object j
+	while(k>i):	#print middle letters of word backwards
+		print(user_input[k])
+		k=k-1
+
+	print(user_input[j])	#print last letter of word
+
+	j=j+2
+	i=j
