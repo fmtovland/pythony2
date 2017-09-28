a337b6d5f09a8a444f3428c734f3ffcaa2d10f9e
Liam Mc Cormick <ljmc2000@gmail.com>
2017-09-28 15:54:42 +0100

    well it seems to work now

--- week2/scramble_word.py
+++ week2/scramble_word.py
@@ -6,22 +6,25 @@
 #python version: 3.4.5
 
 user_input=input("Enter string to have it scrambled\n")	#get user input
+user_input=(user_input+" ..................................")
 i=0	#index of first letter of word
 j=0	#index of space after word
 
 while i < len(user_input):	#program will loop till i reaches end
-	while user_input[j] != ' ' or '.' or ',':	#after this user_input[j] will be the last letter of word
+	while user_input[j] != (' ' or '.' or ','):
 		j=j+1
 	j=j-1
 
-	print(user_input[i])	#print first letter of word
+	print(user_input[i],end='')	#print first letter of word
 
-	k=0+j	#ensure k not directly mapped to object j
+	k=j-1	#ensure k not directly mapped to object j
 	while(k>i):	#print middle letters of word backwards
-		print(user_input[k])
+		print(user_input[k],end='')
 		k=k-1
 
-	print(user_input[j])	#print last letter of word
+	print(user_input[j],end=' ')	#print last letter of word
 
 	j=j+2
-	i=j
+	i=j+0
+	if user_input[j]==".":
+		exit()
