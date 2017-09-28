4fdbf6a4b4caa4a0125571eb7c0a1e14cb8576b9
Liam Mc Cormick <ljmc2000@gmail.com>
2017-09-28 14:25:10 +0100

    went from santax errors to logical ones

--- week2/palendrome.py
+++ week2/palendrome.py
@@ -1,8 +1,8 @@
 word=input("Enter word to check if palendrome: ")
 i=0
-wordlen=(len(word)/2)
+wordlen=int(len(word)/2)
 while(i < wordlen):
-	if word[wordlen-i] != word[i]:
+	if word[wordlen-i] is not word[i]:
 		print("Thats not a palendrome")
 		exit()
 	i=i+1
