0fdb8fc70b6a99bbdf5b2361f4ce16560a9b1568
Liam Mc Cormick <ljmc2000@gmail.com>
2017-09-28 14:54:14 +0100

    completed assignment: now accounts for white space characters and punctuation

--- week2/palendrome.py
+++ week2/palendrome.py
@@ -1,7 +1,18 @@
-word=input("Enter word to check if palendrome: ")
+input_word=input("Enter word to check if palendrome: ")	#get input from user
+input_word=input_word.lower()	#make all lower case
 i=0
-wordlen=len(word)-1	#the -1 accounts for indexes starting at 0
-middle=int(len(word)/2)
+wordlen=len(input_word)
+word=[]			#create blank array
+
+while i < wordlen:	#remove all puncutation
+	if input_word[i].isalpha():
+		word.append(input_word[i])
+	i=i+1
+
+wordlen=len(word)-1     #the -1 accounts for indexes starting at 0
+middle=int(len(word)/2) #calculate index of middle letter
+
+i=0
 while(i < middle):
 	if word[wordlen-i] is not word[i]:
 		print("Thats not a palendrome")
