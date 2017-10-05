53bc70d209e3b9ba0d9ee85f2d56ab3d33a758d2
Liam Mc Cormick <ljmc2000@gmail.com>
2017-10-05 14:11:23 +0100

    added empty abcinfo.py for this week's lab work

---
+++ week3/abcinfo.py
@@ -0,0 +1,11 @@
+#Program requirements: Write a program to read from a file and print
+#the Id, title (first only), time and key signatures line by line.
+#Program author: Liam McCormick
+#Date: 05-10-17
+#OS: Gentoo
+#Kernel: linux 4.13.4 (They finally fixed the dualshock drivers)
+#python version: 3.4.5
+
+#get filename from command line
+import sys
+filename=sys.argv[1]	#make the last argument be the filename
