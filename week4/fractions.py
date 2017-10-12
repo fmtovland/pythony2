c1789b64a8bbc1445020650471ff75f9441ef1a9
Liam Mc Cormick <ljmc2000@gmail.com>
2017-10-12 15:55:24 +0100

    left lab

--- week4/fractions.py
+++ week4/fractions.py
@@ -38,3 +38,17 @@ def lcm(bignum,smallnum):
 	comDivisor=gcd(bignum,smallnum)
 	answer=(bignum*smallnum)/comDivisor
 	return answer
+
+def addFrac(frac1,frac2)
+	'''add two fractions together
+	the fractions should be in the form
+	(a,b) with a as the numberator and b
+	the denominator'''
+
+	commonDenominator=lcm(frac1[1],frac2[1])
+
+	factor1=frac1[1]/commonDenominator	#the number to multiply the numberator and denominator of the first fraction by
+	factor2=frac2[1]/commonDenominator	#	"	"	"	"	"	"	" the second fraction by
+
+	frac1=(frac1[0]*commonDenominator,frac1[1]*commonDenominator)
+	frac2=(frac2[0]*commonDenominator,frac2[1]*commonDenominator)
