2ced6892da82e8e54b595c7ffcea1103568d2dae
Liam Mc Cormick <ljmc2000@gmail.com>
2017-10-12 14:41:27 +0100

    added function bsort to bubble and then moved it to top of code

--- week4/bubble.py
+++ week4/bubble.py
@@ -5,6 +5,21 @@
 #Kernel: Linux 4.13.4
 #Python version: 3.4.5
 
+def bsort(listToSort):
+	'''bubble sort a list then return the sorted list'''
+
+	cont=False
+	while cont==False:
+		cont=False
+		for i in range(0,len(listToSort)-2):	#go through the list once; the -2 is one to account for counting from 0 and one to ensure this process only continues till the penultimate 
+			if listToSort[i] > listToSort[i+1]:
+				tmp=listToSort[i]
+				listToSort[i] = listToSort[i+1]
+				listToSort[i+1]=tmp
+				cont=True
+
+	return listToSort
+
 size=1
 mylist=[]
 tmp=input("Enter numbers line after line: finish with an empty line\n")
@@ -12,8 +27,10 @@ while tmp != '':
 	mylist.append(tmp)
 	tmp=input()
 
-bsort(mylist)	#bsort will be a funtion to bubble sort
+#done with tmp here so safe to use elsewhere
+
+mylist=bsort(mylist)	#bsort will be a funtion to bubble sort
 
 print(mylist)	#print sorted list
 
-#def
+
