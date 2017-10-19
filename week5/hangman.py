0fc4c5f7285299ff08a0b372001a52af44db6706
Liam Mc Cormick <ljmc2000@gmail.com>
2017-10-19 14:53:41 +0100

    added printword to hangman

--- week5/hangman.py
+++ week5/hangman.py
@@ -8,6 +8,9 @@
 #this might have been a terrible idea as the words in this dictionary are not suitable for anyone but a student of english, but whatever
 dicsize=159344	#number of words in dictionary.txt
 
+def drawman(limbs):
+	'''draw a hangman with a specified number of limbs'''
+
 def genword():
 	'''return a random word from dictionary.txt'''
 	from random import randint	#for generating a random number
@@ -23,3 +26,21 @@ def genword():
 		word=word[:-1]	#chop the \n off the end
 
 	return word
+
+def printword(word,guesses):
+	'''print the secret word with non guessed letters censored to _s'''
+	for char in word:
+		if char in guesses:
+			print(char,end='')
+		else:
+			print("_",end='')	#oh dear, an emoji. so sorry!
+	print()
+
+
+secretword=genword()	#generate a word for the user to guess at
+limbs=0			#number of wrong guesses made
+remlets=len(secretword)	#number of letters left to guess
+guesses=[]		#letters that have already been guessed
+
+while limbs>6:		#break out of loop when all limbs have been drawn
+	drawman(limbs)	#draw the hangman (not implemented in this revision)
