0443b8c889775378bc7da9613f9cccb93f304b0d
Liam Mc Cormick <ljmc2000@gmail.com>
2017-11-06 16:40:12 +0000

    extracted mask2list from onnet

--- ipv4tools/ipv4tools.py
+++ ipv4tools/ipv4tools.py
@@ -1,7 +1,6 @@
-def onnet(*address,mask):
-	'''check if an ipv4 address is on a network
-	a b c and d are the octets of the ipv4 address
-	mask is the subnet mask as a number between 0 and 32'''
+def mask2list(mask):
+	'''convert a network mask between 1 and 32 to a list in
+	the form 255.255.0.0'''
 
 	netmask=(2**32) - (2**(32-mask))
 	masklist=[]
@@ -10,6 +9,15 @@ def onnet(*address,mask):
 		masklist.insert(0,netmask%256)
 		netmask=int(netmask/256)
 
+	return masklist
+
+
+def onnet(*address,mask):
+	'''check if an ipv4 address is on a network
+	a b c and d are the octets of the ipv4 address
+	mask is the subnet mask as a number between 0 and 32'''
+
+	masklist=mask2list(mask)
 
 	addresslist=[]
 	for i in range(0,4):
