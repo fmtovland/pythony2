3cb4a0aaa943a8a5eafc39bedd7919ce2df893d2
Liam Mc Cormick <ljmc2000@gmail.com>
2017-11-07 11:49:53 +0000

    removed onnet because it did the same thing as netaddr and the commented out code from broadcastaddr

--- ipv4tools/ipv4tools.py
+++ ipv4tools/ipv4tools.py
@@ -21,19 +21,6 @@ def list2mask(*masklist):
 
 	return int(mask)
 
-def onnet(*address,mask):
-	'''check if an ipv4 address is on a network
-	*address is the octets of the ipv4 address
-	mask is the subnet mask as a number between 0 and 32'''
-
-	masklist=mask2list(mask)
-
-	addresslist=[]
-	for i in range(0,4):
-		addresslist.append(address[i] & masklist[i])
-
-	return addresslist
-
 def broadcast(*address,mask):
 	'''generate a broadcast from any address on a network'''
 
@@ -45,27 +32,6 @@ def broadcast(*address,mask):
 	broadcastaddr=int2addr(broadcastaddrint)
 
 	return broadcastaddr
-
-#
-#	masklist=mask2list(mask)
-#
-#	i=0
-#	addresslist=[]
-#	while(masklist[i]==255 and i<=3):
-#		addresslist.insert(i,address[i])
-#		i=i+1
-#
-#
-#	while i<=3:
-#		addresslist.insert(i,address[i] & masklist[i])
-#		addresslist[i]=addresslist[i] + ((masklist[i]-1)%256)
-#		while addresslist[i]>255:
-#			addresslist[i-1]=addresslist[i-1]+1
-#			addresslist[i]=addresslist[i]-256
-#		i=i+1
-#
-#
-#	return addresslist
 
 def netaddr(*address,mask):
 	'''find the network address of a given ip address'''
