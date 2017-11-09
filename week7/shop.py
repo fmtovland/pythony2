963c9436799b331b49156f3133b735b294395f1a
Liam Mc Cormick <ljmc2000@gmail.com>
2017-11-09 14:39:36 +0000

    added function to save stock to file

--- week7/shop.py
+++ week7/shop.py
@@ -5,10 +5,13 @@
 #Kernel: 4.13.11
 #python version: 3.4.5
 
+#define type variables
+productlist="productlist.txt"	#the filename of the stockfile
+
 def getstock():
 	'''import the stock list'''
 
-	stockfile=open("productlist.txt","r")
+	stockfile=open(productlist.txt,"r")
 
 	mydict={}
 
@@ -24,4 +27,35 @@ def getstock():
 			mydict[id]["price"]=line.strip()[7:]
 			mydict[id]["price"]=float(mydict[id]["price"])
 
+	stockfile.close()
 	return mydict
+
+def savestock(mydict):
+	'''takes a list of remaining stock and saves it to file'''
+
+	stockfile=open(productlist,"w+")
+	for id in mydict:
+		string="id: "+id+"\n"
+		stockfile.write(string)
+
+		string="name: "+mydict[id]["name"]+"\n"
+		stockfile.write(string)
+
+		string="stock:"+mydict[id]["stock"]+"\n"
+		stockfile.write(string)
+
+		string="price:"+str(mydict[id]["price"])+"\n"
+		stockfile.write(string)
+
+		stockfile.write("\n")
+
+	stockfile.close()
+
+def main():
+	'''main block'''
+	availableItems=getstock()	#get a list of all available items
+
+
+
+if __name__ == "__main__":
+	main()
