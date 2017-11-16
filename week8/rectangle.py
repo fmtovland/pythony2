df353574e9c7cbb264b3596634542af39cd47c7a
Liam Mc Cormick <ljmc2000@gmail.com>
2017-11-16 14:12:40 +0000

    added skellington for rectangle class

---
+++ week8/rectangle.py
@@ -0,0 +1,10 @@
+#(1) Write a Python class to represent a Rectangle.
+#In addition to the standard methods (_ _ init_ _, _ _str _ _, get/set)
+#write a method that will calculate the area of a rectangle.
+#Test your class by creating few rectangle objects.
+
+#author: Liam McCormick
+#Date: 16/11/17
+#OS: Gentoo
+#Kernel: 4.13.11
+#Python version: 3.4.5
