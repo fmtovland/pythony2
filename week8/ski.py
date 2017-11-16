b759ae46dc49c6167762c43ca518948a24313388
Liam Mc Cormick <ljmc2000@gmail.com>
2017-11-16 14:36:41 +0000

    added shopping kart item as empty file

---
+++ week8/ski.py
@@ -0,0 +1,7 @@
+#Write a class to represent an Item - each item has item name, price and quantity. Include a method to calculate total price. Test your class by creating few Item objects.
+
+#author: Liam McCormick
+#Date: 16/11/17
+#OS: Gentoo
+#Kernel: 4.13.11
+#python version: 3.4.5
