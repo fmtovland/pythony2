29089a800f9c1f815b800445cb8b153ea7f14572
Liam Mc Cormick <ljmc2000@gmail.com>
2017-11-16 14:54:03 +0000

    added imaginarynum.py with requirements paragraph

---
+++ week8/imaginarynum.py
@@ -0,0 +1,12 @@
+#Write a class to represent a complex number. Complex numbers can
+#be written in the form of a+bi where a and b are real numbers and
+#i is the solution of i^2=-1 Add methods to add and subtract complex
+#numbers - to add/subtract complex numbers you add/subtract the
+#corresponding real and imaginary parts. For example, adding 1+2i
+#and 2+3i will result in 3+5i
+
+#author: Liam McCormick
+#Date: 16/11/17
+#OS: Gentoo
+#Kernel: 4.13.11
+#python version: 3.4.5
